c70415faf4e6def03ad5f29e5a4232c530ab72bd
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-01-08 04:12:25 +0900

    [Silver II] Title: 스택 수열, Time: 372 ms, Memory: 28684 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,55 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class Main {
 
-//    public static final boolean push = true;
-//    public static final boolean pop = false;
-
-//    public static List<Boolean> instructions = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         Stack<Integer> myStack = new Stack<>();
-        List<Integer> sequence = new ArrayList<>();
+//        List<Integer> sequence = new ArrayList<>();
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int size = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < size; i++) {
-            sequence.add(Integer.parseInt(bf.readLine()));
-        }
+//        for (int i = 0; i < size; i++) {
+//            sequence.add(Integer.parseInt(bf.readLine()));
+//        }
         int temp = 0;
         for (int i = 0; i < size; i++) {
-            if (sequence.get(i) >= temp) {
-                for (int j = temp; j < sequence.get(i); j++) {
-//                    instructions.add(push);
-                    sb.append('+').append('\n');
+//            if (sequence.get(i) >= temp) {
+            int i1 = Integer.parseInt(bf.readLine());
+            if (i1 >= temp) {
+                for (int j = temp; j < i1; j++) {
+                    sb.append("+\n");
                     myStack.push(j + 1);
                 }
-                temp = sequence.get(i);
+                temp = i1;
                 myStack.pop();
-                sb.append('-').append('\n');
-//                instructions.add(pop);
-            } else if (sequence.get(i) < temp && myStack.peek().equals(sequence.get(i))) {
+                sb.append("-\n");
+//            } else if (sequence.get(i) < temp && myStack.peek().equals(sequence.get(i))) {
+            } else if (i1 < temp && myStack.peek().equals(i1)) {
                 myStack.pop();
-//                instructions.add(pop);
-                sb.append('-').append('\n');
+                sb.append("-\n");
             } else {
                 System.out.println("NO");
                 return;
             }
         }
         System.out.println(sb);
-//        for (Boolean instruction : instructions) {
-//            if (instruction == push) {
-//                System.out.println("+");
-//            } else {
-//                System.out.println("-");
-//            }
-//        }
 
     }
 }
