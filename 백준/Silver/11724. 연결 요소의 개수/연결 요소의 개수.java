52fae22499e33272e65c9c7a00fdbca8933a806d
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-01-11 19:12:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 680 ms, Memory: 141516 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,15 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     static boolean[] isVisited;
-    static Map<Integer, List<Integer>> nodes = new HashMap<>();
-
+    //    static Map<Integer, List<Integer>> nodes = new HashMap<>();
+    static List<Integer>[] nodes;
     static int connectionGraph = 1;
 
 
@@ -21,28 +19,36 @@ public class Main {
 
         int vertexCount = Integer.parseInt(st.nextToken());
         isVisited = new boolean[vertexCount + 1];
-        int edgeCount = Integer.parseInt(st.nextToken());
 
+        int edgeCount = Integer.parseInt(st.nextToken());
+        nodes = new ArrayList[vertexCount + 1];
         for (int i = 1; i <= vertexCount; i++) {
-            nodes.put(i, new ArrayList<>());
+            nodes[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < edgeCount; i++) {
             st = new StringTokenizer(bf.readLine());
             int vertexOne = Integer.parseInt(st.nextToken());
             int vertexTwo = Integer.parseInt(st.nextToken());
-            nodes.get(vertexOne).add(vertexTwo);
-            nodes.get(vertexTwo).add(vertexOne);
+            nodes[vertexOne].add(vertexTwo);
+            nodes[vertexTwo].add(vertexOne);
         }
 
-        for (Integer i : nodes.keySet()) {
+//        for (Integer i : nodes.keySet()) {
+//            if (!isVisited[i]) {
+//                dfs(i);
+//                if (!isAllCovered()) {
+//                    connectionGraph++;
+//                }
+//            }
+//        }
+        for (int i = 1; i <= vertexCount; i++) {
             if (!isVisited[i]) {
                 dfs(i);
                 if (!isAllCovered()) {
                     connectionGraph++;
                 }
             }
-
         }
         StringBuilder sb = new StringBuilder();
         sb.append(connectionGraph);
@@ -53,7 +59,7 @@ public class Main {
 
     public static void dfs(int startIndex) {
         isVisited[startIndex] = true;
-        List<Integer> integers = nodes.get(startIndex);
+        List<Integer> integers = nodes[startIndex];
         for (Integer nextNode : integers) {
             if (!isVisited[nextNode]) {
                 dfs(nextNode);
