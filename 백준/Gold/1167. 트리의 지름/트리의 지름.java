8f2c6a9d7dec5a76ec25ec187d84635b80e29731
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-01-26 00:20:27 +0900

    [Gold II] Title: 트리의 지름, Time: 968 ms, Memory: 106864 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static Map<Integer, Integer>[] adjustNodes;
+
+    static boolean[] visited;
+
+    static int longestNode;
+
+    static int diameter = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        adjustNodes = new HashMap[Integer.parseInt(bf.readLine()) + 1];
+        for (int i = 0; i < adjustNodes.length; i++) {
+            adjustNodes[i] = new HashMap<>();
+        }
+        visited = new boolean[adjustNodes.length + 1];
+
+        settingAdjustNodes(bf);
+        dfs(1, 0);
+        visited = new boolean[adjustNodes.length + 1];
+        dfs(longestNode, 0);
+
+        System.out.println(diameter);
+    }
+
+    private static void settingAdjustNodes(BufferedReader bf) throws IOException {
+        for (int i = 0; i < adjustNodes.length - 1; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int startNodeIndex = Integer.parseInt(st.nextToken());
+            while (true) {
+                int nodeIndex = Integer.parseInt(st.nextToken());
+
+                if (nodeIndex == -1) {
+                    break;
+                }
+                int adjustLength = Integer.parseInt(st.nextToken());
+                adjustNodes[startNodeIndex].put(nodeIndex, adjustLength);
+//                adjustNodes[nodeIndex].put(startNodeIndex, adjustLength);
+            }
+        }
+    }
+
+    private static void dfs(int node, int nowDiameter) {
+
+        visited[node] = true;
+        Map<Integer, Integer> adjustNode = adjustNodes[node];
+        for (Entry<Integer, Integer> adjustNodeIndex : adjustNode.entrySet()) {
+            if (!visited[adjustNodeIndex.getKey()]) {
+                dfs(adjustNodeIndex.getKey(), nowDiameter + adjustNodeIndex.getValue());
+            }
+        }
+        if (nowDiameter > diameter) {
+            diameter = nowDiameter;
+            longestNode = node;
+        }
+    }
+
+    private static int findFirstLeaf(int node) {
+
+        visited[node] = true;
+        Map<Integer, Integer> adjustNode = adjustNodes[node];
+        for (Entry<Integer, Integer> adjustNodeIndex : adjustNode.entrySet()) {
+            if (!visited[adjustNodeIndex.getKey()]) {
+                findFirstLeaf(adjustNodeIndex.getKey());
+            }
+        }
+        return node;
+    }
+
+
+}
