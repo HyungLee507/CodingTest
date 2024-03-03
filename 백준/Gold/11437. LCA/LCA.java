d5e033b87ec7229d54d381792d5b0fd1424e0578
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-03-03 19:00:37 +0900

    [Gold III] Title: LCA, Time: 1212 ms, Memory: 44768 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -2,72 +2,109 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
 
-    static int N, M;
-    static List<Integer>[] graph;
-    static int[] parents;
-    static int[] depth;
+    static ArrayList<Integer>[] adjustNodes;
     static boolean[] visited;
+    static int[] parents;
+    static int[] depths;
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        graph = new List[N + 1];
-        for (int n = 1; n <= N; n++) {
-            graph[n] = new ArrayList<>();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int nodeCount = Integer.parseInt(bf.readLine());
+        visited = new boolean[nodeCount + 1];
+        parents = new int[nodeCount + 1];
+        depths = new int[nodeCount + 1];
+        adjustNodes = new ArrayList[nodeCount + 1];
+        for (int i = 0; i < nodeCount + 1; i++) {
+            adjustNodes[i] = new ArrayList<>();
         }
-        parents = new int[N + 1];
-        depth = new int[N + 1];
-        visited = new boolean[N + 1];
-
-        for (int n = 0; n < N - 1; n++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a].add(b);
-            graph[b].add(a);
+        for (int i = 0; i < nodeCount - 1; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            adjustNodes[node1].add(node2);
+            adjustNodes[node2].add(node1);
         }
-
         dfs(1, 0);
-
-        M = Integer.parseInt(br.readLine());
-        for (int m = 0; m < M; m++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            while (a != b) {
-                if (depth[a] == depth[b]) {
-                    a = parents[a];
-                    b = parents[b];
-                } else if (depth[a] > depth[b]) {
-                    a = parents[a];
-                } else {
-                    b = parents[b];
-                }
-            }
-
-            sb.append(a).append('\n');
+//        bfs(1);
+        int lcaCount = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < lcaCount; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            int commonAncestor = lca(node1, node2);
+            sb.append(commonAncestor).append('\n');
         }
-
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    static void dfs(int v, int d) {
-        depth[v] = d;
-        visited[v] = true;
-
-        for (int child : graph[v]) {
-            if (visited[child]) {
-                continue;
+    public static int lca(int a, int b) {
+        while (a != b) {
+            if (depths[a] == depths[b]) {
+                a = parents[a];
+                b = parents[b];
+            } else if (depths[a] > depths[b]) {
+                a = parents[a];
+            } else {
+                b = parents[b];
             }
-            parents[child] = v;
-            dfs(child, d + 1);
+        }
+        return a;
+//        if (parents[node1] < parents[node2]) {
+//            int temp = node1;
+//            node1 = node2;
+//            node2 = temp;
+//        }
+//        while (depths[node1] != depths[node2]) {
+//            node1 = parents[node1];
+//        }
+//        while (node1 != node2) {
+//            node1 = parents[node1];
+//            node2 = parents[node2];
+//        }
+//        return node1;
+    }
+
+    public static void dfs(int index, int depth) {
+        depths[index] = depth;
+        visited[index] = true;
+        for (int i : adjustNodes[index]) {
+            if (!visited[i]) {
+                parents[i] = index;
+                dfs(i, depth + 1);
+            }
         }
     }
+
+//    public static void bfs(int node) {
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        queue.add(node);
+//        visited[node] = true;
+//        int level = 1;
+//        int now_size = 1;
+//        int count = 0;
+//        while (!queue.isEmpty()) {
+//            int now_node = queue.poll();
+//            for (Integer next : adjustNodes[now_node]) {
+//                if (!visited[next]) {
+//                    visited[next] = true;
+//                    queue.add(next);
+//                    parents[next] = now_node;
+//                    depths[next] = level;
+//                }
+//            }
+//            count++;
+//            if (count == now_size) {
+//                count = 0;
+//                now_size = queue.size();
+//                level++;
+//            }
+//        }
+//    }
+
 }
