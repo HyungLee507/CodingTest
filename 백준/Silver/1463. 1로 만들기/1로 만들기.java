2a291928a6731150a8653c4391417f54ffe43a92
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-03-07 16:45:54 +0900

    [Silver III] Title: 1로 만들기, Time: 160 ms, Memory: 18436 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(bf.readLine());
+        int[] numbers = initNumbers(number);
+        System.out.println(numbers[number]);
+    }
+
+    private static int[] initNumbers(int number) {
+        int[] numbers = new int[number + 1];
+        numbers[1] = 0;
+        if (number == 2) {
+            numbers[2] = 1;
+        }
+        if (number >= 3) {
+            numbers[2] = 1;
+            numbers[3] = 1;
+        }
+
+        for (int i = 4; i < numbers.length; i++) {
+            numbers[i] = Integer.min(numbers[i - 1] + 1, numbers[i - 2] + 2);
+            if (i % 6 == 0) {
+                numbers[i] = Integer.min(numbers[i], numbers[i / 3] + 1);
+                numbers[i] = Integer.min(numbers[i], numbers[i / 2] + 1);
+            } else if (i % 3 == 0) {
+                numbers[i] = Integer.min(numbers[i], numbers[i / 3] + 1);
+            } else if (i % 2 == 0) {
+                numbers[i] = Integer.min(numbers[i], numbers[i / 2] + 1);
+            }
+        }
+        return numbers;
+    }
+
+}
