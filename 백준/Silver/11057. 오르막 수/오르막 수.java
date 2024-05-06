19270cb16dd219f23c31a050bd7118a13debeb08
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-05-07 01:22:11 +0900

    [Silver I] Title: 오르막 수, Time: 136 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        long[][] dp = new long[n + 1][10];
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] += (dp[i - 1][k]) % 10007;
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp[n]).sum() % 10007);
+
+    }
+}
