a2bd0b196461f1d1d8abc5661ba73587807317c8
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-05-13 23:08:27 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 68.74 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,27 +1,54 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
-        Set<String> set1 = new HashSet<>(List.of(gems));
-        Set<String> set2 = new HashSet<>();
-        Map<String, Integer> map = new HashMap<>();
-        int l = 0;
-        int r = 0;
-        int len = Integer.MAX_VALUE;
-        int[] answer = new int[2];
-        while (r < gems.length) {
-            map.put(gems[r], map.getOrDefault(gems[r], 0) + 1);
-            set2.add(gems[r]);
-            while (map.get(gems[l]) > 1) {
-                map.replace(gems[l], map.get(gems[l]) - 1);
-                l++;
+        List<String> list = Arrays.asList(gems);
+        Set<String> gemSet = new HashSet<>(list);
+        int markSize = gemSet.size();
+        int[] answer = new int[]{1, gems.length};
+        int start = 0;
+        int end = 0;
+        Map<String, Integer> markMap = new HashMap();
+        while (start <= end && end < gems.length) {
+            int size = markMap.size();
+            if (size < markSize) {
+                markMap.put(gems[end], markMap.getOrDefault(gems[end], 0) + 1);
+                if (markMap.size() != markSize) {
+                    end++;
+                }
+            } else {
+                removeData(markMap, gems[start]);
+                answer = updateIndex(start, end, answer);
+                if (markMap.size()!= markSize) {
+                    end++;
+                }
+                start++;
             }
-            if (r - l < len && set1.size() == set2.size()) {
-                answer[0] = l + 1;
-                answer[1] = r + 1;
-                len = r - l;
-            }
-            r++;
         }
         return answer;
+    }
+    private int[] updateIndex(int start, int end, int[] answer) {
+        if ((end - start) >= answer[1] - answer[0]) {
+            return answer;
+        } else {
+            return new int[]{start + 1, end + 1};
+        }
+    }
+    private void removeData(Map<String, Integer> markMap, String gem) {
+        int value = markMap.get(gem);
+        if(value ==1){
+            markMap.remove(gem);
+            return;
+        }
+        markMap.put(gem, value - 1);
+    }
+
+    private int check(Map<String, Integer> markMap) {
+        int gemCount = 0;
+        for (int value : markMap.values()) {
+            if (value > 0) {
+                gemCount++;
+            }
+        }
+        return gemCount;
     }
 }
