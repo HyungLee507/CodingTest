9b443bc739974fc93ae00b0dfc8dc93be6e86c05
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2024-09-28 20:56:47 +0900

    [level 4] Title: 가사 검색, Time: 1036.04 ms, Memory: 820 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/README.md
+++ 프로그래머스/4/60060. 가사 검색/README.md
@@ -1,10 +1,10 @@
 # [level 4] 가사 검색 - 60060 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60060?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60060) 
 
 ### 성능 요약
 
-메모리: 889 MB, 시간: 1247.70 ms
+메모리: 820 MB, 시간: 1036.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 18:09:23
+2024년 09월 28일 20:56:44
 
 ### 문제 설명
 
--- 프로그래머스/4/60060. 가사 검색/가사 검색.java
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.java
@@ -1,78 +1,51 @@
-import java.util.HashMap;
-import java.util.Map;
-
-class TrieNode {
-    Map<Character, TrieNode> children;
-    Map<Integer, Integer> lengthCount; // 해당 길이의 단어 개수 저장
-    
-    TrieNode() {
-        children = new HashMap<>();
-        lengthCount = new HashMap<>();
-    }
-}
-
-class Trie {
-    private TrieNode root;
-    
-    Trie() {
-        root = new TrieNode();
-    }
-    
-    // 단어를 트라이에 삽입하면서 각 노드의 lengthCount를 갱신
-    public void insert(String word) {
-        TrieNode node = root;
-        int length = word.length();
-        
-        for (char ch : word.toCharArray()) {
-            node.lengthCount.put(length, node.lengthCount.getOrDefault(length, 0) + 1);
-            node.children.putIfAbsent(ch, new TrieNode());
-            node = node.children.get(ch);
-        }
-        node.lengthCount.put(length, node.lengthCount.getOrDefault(length, 0) + 1); // 마지막 노드도 갱신
-    }
-    
-    // 쿼리에 맞는 단어 개수 찾기
-    public int countMatches(String query) {
-        TrieNode node = root;
-        int length = query.length(); // 쿼리의 길이
-        
-        for (char ch : query.toCharArray()) {
-            if (ch == '?') {
-                return node.lengthCount.getOrDefault(length, 0); // 해당 길이에 해당하는 단어 개수 반환
-            }
-            if (!node.children.containsKey(ch)) {
-                return 0; // 매칭되는 단어가 없으면 0 반환
-            }
-            node = node.children.get(ch);
-        }
-        return node.lengthCount.getOrDefault(length, 0); // 해당 길이에 해당하는 단어 개수 반환
-    }
-}
+import java.util.*;
 
 class Solution {
-    public int[] solution(String[] words, String[] queries) {
-        Trie trie = new Trie();
-        Trie reverseTrie = new Trie();
-        
-        for (String word : words) {
-            trie.insert(word); // 정방향 삽입
-            reverseTrie.insert(new StringBuilder(word).reverse().toString()); // 역방향 삽입
-        }
-        
-        int[] result = new int[queries.length];
-        
-        for (int i = 0; i < queries.length; i++) {
-            String query = queries[i];
-            if (query.charAt(0) == '?') {
-                // 접미사 패턴일 경우
-                String reversedQuery = new StringBuilder(query).reverse().toString();
-                result[i] = reverseTrie.countMatches(reversedQuery);
-            } else {
-                // 접두사 패턴일 경우
-                result[i] = trie.countMatches(query);
+    class Trie {
+        Map<Integer, Integer> lenMap = new HashMap<>();
+        Trie[] child = new Trie[26];
+
+        void insert(String str) {
+            Trie node = this;
+            int len = str.length();
+            lenMap.put(len, lenMap.getOrDefault(len, 0) + 1);
+
+            for (char ch : str.toCharArray()) {
+                int idx = ch - 'a';
+                if (node.child[idx] == null)
+                    node.child[idx] = new Trie();
+
+                node = node.child[idx];
+                node.lenMap.put(len, node.lenMap.getOrDefault(len, 0) + 1);
             }
         }
-        
-        return result;
+
+        int find(String str, int i) {
+            if (str.charAt(i) == '?')
+                return lenMap.getOrDefault(str.length(), 0);
+
+            int idx = str.charAt(i) - 'a';
+            return child[idx] == null ? 0 : child[idx].find(str, i + 1);
+        }
+
+    }
+
+    public int[] solution(String[] words, String[] queries) {
+        Trie front = new Trie();
+        Trie back = new Trie();
+
+        for (String word : words) {
+            front.insert(word);
+            back.insert(reverse(word));
+        }
+
+        return Arrays.stream(queries).mapToInt(
+                query -> query.charAt(0) == '?' ?
+                        back.find(reverse(query), 0) :
+                        front.find(query, 0)).toArray();
+    }
+    
+    String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
     }
 }
