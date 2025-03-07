41ed2098bfbab1f2a75cc0b551a333fa24dc578e
HyunLee <101203777+HyungLee507@users.noreply.github.com>
2025-03-08 01:30:50 +0900

    [Gold I] Title: 구간 합 구하기, Time: 540 ms, Memory: 103128 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,102 +1,87 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static long[] numbers;
+    static long[] tree, arr;
+    static int n, m, k;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
-        int numberCount = Integer.parseInt(st.nextToken());
-        int changeCount = Integer.parseInt(st.nextToken());
-        int intervalSumCount = Integer.parseInt(st.nextToken());
-        int temp = numberCount;
-        int numbersSize = 2;
-        while (temp != 0) {
-//            if (temp > 0) {
-//                temp = temp / 2;
-//                numbersSize *= 2;
-//            }
-            temp = temp / 2;
-            numbersSize *= 2;
-        }
-        numbers = new long[numbersSize];
-
-        for (int i = 0; i < numberCount; i++) {
-            long number = Long.parseLong(bf.readLine());
-            numbers[numbersSize / 2 + i] = number;
-        }
-
-        settingTree();
-
         StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < intervalSumCount + changeCount; i++) {
-            st = new StringTokenizer(bf.readLine());
-            int command = Integer.parseInt(st.nextToken());
-            long value1 = Long.parseLong(st.nextToken());
-            long value2 = Long.parseLong(st.nextToken());
-
-            if (command == 1) {
-                // 세그먼트 트리 구간 합 세팅
-//                numbers[(int) (numbersSize / 2 + value1 - 1)] = value2;
-                changeValue(value1, value2);
-            }
-            if (command == 2) {
-                // 세그먼트 트리를 활용한 구간 합 출력
-                sb.append(getSum(value1, value2)).append('\n');
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        arr = new long[n + 1];
+        int H = (int) (Math.ceil(Math.log(n) / Math.log(2)));
+        tree = new long[1 << (H + 1)];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Long.parseLong(bf.readLine());
+        }
+        init(1, 1, n);
+        for (int i = 0; i < m + k; i++) {
+            st = new StringTokenizer(bf.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            switch (a) {
+                case 1:
+                    long val = Long.parseLong(st.nextToken());
+                    update(1, 1, n, b, val);
+                    break;
+                case 2:
+                    int c = Integer.parseInt(st.nextToken());
+                    sb.append(get(1, 1, n, b, c)).append('\n');
+                    break;
             }
         }
         System.out.println(sb);
-
-
+        bf.close();
     }
 
-    private static long getSum(long start, long end) {
-
-        long sum = 0;
-        start = numbers.length / 2 - 1 + start;
-        end = numbers.length / 2 - 1 + end;
-        while (start <= end) {
-            if (start % 2 == 1) {
-                sum += numbers[(int) start];
-                start++;
-            }
-            if (end % 2 == 0) {
-                sum += numbers[(int) end];
-                end--;
-            }
-            start = (start) / 2;
-            end = (end) / 2;
+    private static void init(int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+            return;
         }
-
-        return sum;
-
+        int mid = (start + end) / 2;
+        init(2 * node, start, mid);
+        init(2 * node + 1, mid + 1, end);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
     }
 
-    private static void settingTree() {
-
-        int temp = numbers.length / 2;
-        while (temp > 1) {
-            for (int i = temp / 2; i < temp; i++) {
-                numbers[i] = numbers[i * 2] + numbers[i * 2 + 1];
-            }
-            temp /= 2;
+    private static long get(int node, int start, int end, int left, int right) {
+        if (right < start || end < left) {
+            return 0;
         }
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        long lsum = get(2 * node, start, mid, left, right);
+        long rsum = get(2 * node + 1, mid + 1, end, left, right);
+        return lsum + rsum;
     }
 
-    private static void changeValue(long index, long value) {
-
-        index = numbers.length / 2 + index - 1;
-        long diff = numbers[(int) index] - value;
-        numbers[(int) index] = value;
-        int temp = (int) (index / 2);
-        while (temp >= 1) {
-            numbers[temp] -= diff;
-            temp /= 2;
+    private static void updateTree(int node, int start, int end, int index, long diff) {
+        if (index < start || end < index) {
+            return;
         }
+        if (start == end) {
+            tree[node] += diff;
+            return;
+        }
+        int mid = (start + end) / 2;
+        updateTree(2 * node, start, mid, index, diff);
+        updateTree(2 * node + 1, mid + 1, end, index, diff);
+        tree[node] = tree[2 * node] + tree[2 * node + 1];
+    }
+
+    private static void update(int node, int start, int end, int index, long val) {
+        long diff = (val - arr[index]);
+        arr[index] = val;
+        updateTree(node, start, end, index, diff);
     }
 }
